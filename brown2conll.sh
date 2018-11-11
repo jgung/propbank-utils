7a0225652d1112311cc4b83b39735d2e4e218d98
James Gung <James.Gung@ipsoft.com>
2018-11-11 16:36:15 -0700

    Cleanup scripts

--- brown2conll.sh
+++ brown2conll.sh
@@ -1,49 +1,49 @@
 #!/bin/bash
 
-PROGRAM_NAME=$0
+program_name=$0
 
 function usage()
 {
     echo "Download and convert Brown corpus data to VN roles and PB roles."
     echo "Requires PTB dataset from https://catalog.ldc.upenn.edu/ldc99t42 and PropBanked Brown corpus pointers"
     echo ""
-    echo "$PROGRAM_NAME path/to/ptb path/to/brown.props"
+    echo "$program_name path/to/ptb path/to/brown.props"
     echo -e "\t-h --help"
     echo -e "\tpath/to/ptb\tPath to treebank_3/parsed/mrg directory of Penn TreeBank -- LDC99T42"
     echo -e "\tpath/to/brown.props\tPath to PropBanked Brown corpus pointers"
 }
 
-if [ -z "$1" ] || [ -z "$2"] ; then
+if [[ -z "$1" ]] || [[ -z "$2" ]] ; then
     usage
     exit 1
 fi
 
-OUTPUT_PATH=`pwd`
-PTB_DIR=${1%/}
-BROWN=$2
-SEMLINK=semlink1.1
+output_path=`pwd`
+ptb_dir=${1%/}
+brown=$2
+semlink=semlink1.1
 
 # Download data
 checkdownload() {
-    if [ ! -f "$OUTPUT_PATH/$1" ]; then
-        wget -O "$OUTPUT_PATH/$1" $2
+    if [[ ! -f "$output_path/$1" ]]; then
+        wget -O "$output_path/$1" $2
     else
         echo "File $1 already exists, skipping download"
     fi
-    tar xf "$OUTPUT_PATH/$1" -C "$OUTPUT_PATH"
+    tar xf "$output_path/$1" -C "$output_path"
 }
 
 checkdownload srlconll-1.1.tgz http://www.lsi.upc.edu/~srlconll/srlconll-1.1.tgz
-checkdownload $SEMLINK.tar.gz https://verbs.colorado.edu/semlink/versions/$SEMLINK.tar.gz
+checkdownload ${semlink}.tar.gz https://verbs.colorado.edu/semlink/versions/${semlink}.tar.gz
 
-python pb_process.py --pb $BROWN --semlink-mappings $SEMLINK/vn-pb/type_map.xml --filter-incomplete --vn --vncls \
---sort-columns 0,1,2 --o $BROWN.vn.props
-python pb2conll.py --pb $BROWN.vn.props --tb $PTB_DIR --combined $BROWN.vn.txt --all --include-inputs
+python pb_process.py --pb ${brown} --semlink-mappings ${semlink}/vn-pb/type_map.xml --filter-incomplete --vn --vncls \
+--sort-columns 0,1,2 --o ${brown}.vn.props
+python pb2conll.py --pb ${brown}.vn.props --tb ${ptb_dir} --combined ${brown}.vn.txt --all --include-inputs
 
-python pb_process.py --pb $BROWN --semlink-mappings $SEMLINK/vn-pb/type_map.xml --filter-incomplete \
---sort-columns 0,1,2 --o $BROWN.pb.props
-python pb2conll.py --pb $BROWN.pb.props --tb $PTB_DIR --combined $BROWN.pb.txt --all --include-inputs
+python pb_process.py --pb ${brown} --semlink-mappings ${semlink}/vn-pb/type_map.xml --filter-incomplete \
+--sort-columns 0,1,2 --o ${brown}.pb.props
+python pb2conll.py --pb ${brown}.pb.props --tb ${ptb_dir} --combined ${brown}.pb.txt --all --include-inputs
 
-python pb_process.py --pb $BROWN --semlink-mappings $SEMLINK/vn-pb/type_map.xml --filter-incomplete --vncls \
---sort-columns 0,1,2 --o $BROWN.pbvn.props
-python pb2conll.py --pb $BROWN.pbvn.props --tb $PTB_DIR --combined $BROWN.pbvn.txt --all --include-inputs
+python pb_process.py --pb ${brown} --semlink-mappings ${semlink}/vn-pb/type_map.xml --filter-incomplete --vncls \
+--sort-columns 0,1,2 --o ${brown}.pbvn.props
+python pb2conll.py --pb ${brown}.pbvn.props --tb ${ptb_dir} --combined ${brown}.pbvn.txt --all --include-inputs
--- semlink2conll.sh
+++ semlink2conll.sh
@@ -1,67 +1,67 @@
 #!/bin/bash
 
-PROGRAM_NAME=$0
+program_name=$0
 
 function usage()
 {
     echo "Download and convert SemLink data to VN roles and PB roles."
     echo "Requires PTB dataset from https://catalog.ldc.upenn.edu/ldc99t42."
     echo ""
-    echo "$PROGRAM_NAME path/to/ptb"
+    echo "$program_name path/to/ptb"
     echo -e "\t-h --help"
     echo -e "\tpath/to/ptb\tPath to treebank_3/parsed/mrg directory of Penn TreeBank -- LDC99T42"
 }
 
-if [ -z "$1" ]; then
+if [[ -z "$1" ]]; then
     usage
     exit 1
 fi
 
-OUTPUT_PATH=`pwd`
-PTB_DIR=${1%/}
-SEMLINK=semlink1.1
+output_path=`pwd`
+ptb_dir=${1%/}
+semlink=semlink1.1
 
 # Download data
 checkdownload() {
-    if [ ! -f "$OUTPUT_PATH/$1" ]; then
-        wget -O "$OUTPUT_PATH/$1" $2
+    if [[ ! -f "$output_path/$1" ]]; then
+        wget -O "$output_path/$1" $2
     else
         echo "File $1 already exists, skipping download"
     fi
-    tar xf "$OUTPUT_PATH/$1" -C "$OUTPUT_PATH"
+    tar xf "$output_path/$1" -C "$output_path"
 }
 
 checkdownload srlconll-1.1.tgz http://www.lsi.upc.edu/~srlconll/srlconll-1.1.tgz
-checkdownload $SEMLINK.tar.gz https://verbs.colorado.edu/semlink/versions/$SEMLINK.tar.gz
+checkdownload ${semlink}.tar.gz https://verbs.colorado.edu/semlink/versions/${semlink}.tar.gz
 
-SCRIPT_PATH="scripts/link_tbpb_vn.pl"
+script_path="scripts/link_tbpb_vn.pl"
 
 echo "Creating train/valid/test split with VN roles and VN senses"
-python pb_process.py --pb $SEMLINK/vn-pb/vnpbprop.txt --semlink --filter-incomplete --vn --vncls --sort-columns 0,1,2 --o $SEMLINK/vnprop.txt
+python pb_process.py --pb ${semlink}/vn-pb/vnpbprop.txt --semlink --filter-incomplete --vn --vncls --sort-columns 0,1,2 --o ${semlink}/vnprop.txt
 
-python pb2conll.py --pb $SEMLINK/vnprop.txt --tb $PTB_DIR --o $SEMLINK/vnprops \
---filter ".*WSJ/(0[2-9]|1[0-9]|2[01])/.*" --combined $SEMLINK/vn-train.txt --all --include-inputs --script $SCRIPT_PATH
-python pb2conll.py --pb $SEMLINK/vnprop.txt --tb $PTB_DIR --o $SEMLINK/vnprops \
---filter .*WSJ/24/.* --combined $SEMLINK/vn-valid.txt --all --include-inputs --script $SCRIPT_PATH
-python pb2conll.py --pb $SEMLINK/vnprop.txt --tb $PTB_DIR --o $SEMLINK/vnprops \
---filter .*WSJ/23/.* --combined $SEMLINK/vn-test.txt --all --include-inputs --script $SCRIPT_PATH
+python pb2conll.py --pb ${semlink}/vnprop.txt --tb ${ptb_dir} --o ${semlink}/vnprops \
+--filter ".*WSJ/(0[2-9]|1[0-9]|2[01])/.*" --combined ${semlink}/vn-train.txt --all --include-inputs --script ${script_path}
+python pb2conll.py --pb ${semlink}/vnprop.txt --tb ${ptb_dir} --o ${semlink}/vnprops \
+--filter .*WSJ/24/.* --combined ${semlink}/vn-valid.txt --all --include-inputs --script ${script_path}
+python pb2conll.py --pb ${semlink}/vnprop.txt --tb ${ptb_dir} --o ${semlink}/vnprops \
+--filter .*WSJ/23/.* --combined ${semlink}/vn-test.txt --all --include-inputs --script ${script_path}
 
 echo "Creating train/valid/test split with PB roles"
-python pb_process.py --pb $SEMLINK/vn-pb/vnpbprop.txt --semlink --filter-incomplete --sort-columns 0,1,2 --o $SEMLINK/pbprop.txt
+python pb_process.py --pb ${semlink}/vn-pb/vnpbprop.txt --semlink --filter-incomplete --sort-columns 0,1,2 --o ${semlink}/pbprop.txt
 
-python pb2conll.py --pb $SEMLINK/pbprop.txt --tb $PTB_DIR --o $SEMLINK/pbprops \
---filter ".*WSJ/(0[2-9]|1[0-9]|2[01])/.*" --combined $SEMLINK/pb-train.txt --all --include-inputs --script $SCRIPT_PATH
-python pb2conll.py --pb $SEMLINK/pbprop.txt --tb $PTB_DIR --o $SEMLINK/pbprops \
---filter .*WSJ/24/.* --combined $SEMLINK/pb-valid.txt --all --include-inputs --script $SCRIPT_PATH
-python pb2conll.py --pb $SEMLINK/pbprop.txt --tb $PTB_DIR --o $SEMLINK/pbprops \
---filter .*WSJ/23/.* --combined $SEMLINK/pb-test.txt --all --include-inputs --script $SCRIPT_PATH
+python pb2conll.py --pb ${semlink}/pbprop.txt --tb ${ptb_dir} --o ${semlink}/pbprops \
+--filter ".*WSJ/(0[2-9]|1[0-9]|2[01])/.*" --combined ${semlink}/pb-train.txt --all --include-inputs --script ${script_path}
+python pb2conll.py --pb ${semlink}/pbprop.txt --tb ${ptb_dir} --o ${semlink}/pbprops \
+--filter .*WSJ/24/.* --combined ${semlink}/pb-valid.txt --all --include-inputs --script ${script_path}
+python pb2conll.py --pb ${semlink}/pbprop.txt --tb ${ptb_dir} --o ${semlink}/pbprops \
+--filter .*WSJ/23/.* --combined ${semlink}/pb-test.txt --all --include-inputs --script ${script_path}
 
 echo "Creating train/valid/test split with PB roles and VN senses"
-python pb_process.py --pb $SEMLINK/vn-pb/vnpbprop.txt --semlink --filter-incomplete --vncls --sort-columns 0,1,2 --o $SEMLINK/pbvnprop.txt
+python pb_process.py --pb ${semlink}/vn-pb/vnpbprop.txt --semlink --filter-incomplete --vncls --sort-columns 0,1,2 --o ${semlink}/pbvnprop.txt
 
-python pb2conll.py --pb $SEMLINK/pbvnprop.txt --tb $PTB_DIR --o $SEMLINK/pbprops \
---filter ".*WSJ/(0[2-9]|1[0-9]|2[01])/.*" --combined $SEMLINK/pbvn-train.txt --all --include-inputs --script $SCRIPT_PATH
-python pb2conll.py --pb $SEMLINK/pbvnprop.txt --tb $PTB_DIR --o $SEMLINK/pbprops \
---filter .*WSJ/24/.* --combined $SEMLINK/pbvn-valid.txt --all --include-inputs --script $SCRIPT_PATH
-python pb2conll.py --pb $SEMLINK/pbvnprop.txt --tb $PTB_DIR --o $SEMLINK/pbprops \
---filter .*WSJ/23/.* --combined $SEMLINK/pbvn-test.txt --all --include-inputs --script $SCRIPT_PATH
+python pb2conll.py --pb ${semlink}/pbvnprop.txt --tb ${ptb_dir} --o ${semlink}/pbprops \
+--filter ".*WSJ/(0[2-9]|1[0-9]|2[01])/.*" --combined ${semlink}/pbvn-train.txt --all --include-inputs --script ${script_path}
+python pb2conll.py --pb ${semlink}/pbvnprop.txt --tb ${ptb_dir} --o ${semlink}/pbprops \
+--filter .*WSJ/24/.* --combined ${semlink}/pbvn-valid.txt --all --include-inputs --script ${script_path}
+python pb2conll.py --pb ${semlink}/pbvnprop.txt --tb ${ptb_dir} --o ${semlink}/pbprops \
+--filter .*WSJ/23/.* --combined ${semlink}/pbvn-test.txt --all --include-inputs --script ${script_path}
